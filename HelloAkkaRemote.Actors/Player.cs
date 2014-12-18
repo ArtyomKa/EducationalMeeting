93a933c31439bb21438749048aceca7329dfd9a9
Artyom Katsap <artyom.katsap@picscout.com>
2014-12-18 15:12:17 +0200

    Added RemoteActors Project

---
+++ HelloAkkaRemote.Actors/Player.cs
@@ -0,0 +1,12 @@
+﻿using Akka.Actor;
+
+namespace HelloAkkaRemote.Actors
+{
+    public class Player : ReceiveActor
+    {
+        public Player()
+        {
+            Receive<string>(s => s == "ping", _ => Sender.Tell("pong"));
+        }
+    }
+}
