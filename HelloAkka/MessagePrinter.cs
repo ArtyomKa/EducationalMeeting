7d25bde51cab2afc0e769a6c82f258ca7154a0ab
Artyom Katsap <artyom.katsap@gmail.com>
2014-12-28 09:45:43 -0800

    reimplemented  hello akka

---
+++ HelloAkka/MessagePrinter.cs
@@ -0,0 +1,19 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace HelloAkka
+{
+    class MessagePrinter : ReceiveActor
+    {
+        
+        public MessagePrinter()
+        {
+            Receive<Message>(message => Console.WriteLine("{0} Received {1}", Thread.CurrentThread.ManagedThreadId, message.Text));
+        }
+
+    }
+}
--- HelloAkka/Program.cs
+++ HelloAkka/Program.cs
@@ -4,21 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Threading;
 namespace HelloAkka
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var system = ActorSystem.Create("Hello");
-            var playerOne = system.ActorOf(Props.Create<Player>(()=>new Player(1)));
-            var playerTwo = system.ActorOf(Props.Create<Player>(() => new Player(2)));
-            playerOne.Tell(new Begin { SecondPlayer = playerTwo});
+            ActorSystem system = ActorSystem.Create("Hello");
+            var playerOne = system.ActorOf(Props.Create<MessagePrinter>(),"MessagePrinter");
+
+            string input = "";
+            while(!(input = Console.ReadLine()).Equals("quit"))
+            {
+                Console.WriteLine("Sending {0} from thread {1}", input,Thread.CurrentThread.ManagedThreadId);
+                playerOne.Tell(new Message(input));
+                
+            }
+            
 
 
             
-            Console.ReadLine();
         }
     }
 }
